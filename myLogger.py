7d978dc1dc102e10a906274b5c2806ec85bf17a4
Dburris13 <sirrub95@protonmail.com>
2023-11-12 00:56:18 +0000

    Changin myLogger.py default filename from WAREAGLE_workshop8.log to WAREAGLE_sqa2023_project.log

--- myLogger.py
+++ myLogger.py
@@ -1,7 +1,7 @@
 import logging
 
 def createLoggerObj(): 
-    fileName  = 'WAREAGLE_workshop8.log' 
+    fileName  = 'WAREAGLE_sqa2023_project.log' 
     formatStr = '%(asctime)s %(message)s'
     logging.basicConfig(format=formatStr, filename=fileName, level=logging.INFO)
     myLogObj = logging.getLogger('sqa2023-logger') 
