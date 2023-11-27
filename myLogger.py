9d44d918507cf619ff74916dbef103c801365fc1
Dburris13 <sirrub95@protonmail.com>
2023-11-27 04:58:34 +0000

    Removing method from myLogger.py so we can just import as global object

--- myLogger.py
+++ myLogger.py
@@ -1,8 +1,7 @@
 import logging
 
-def createLoggerObj(): 
-    fileName  = 'WAREAGLE_sqa2023_project.log' 
-    formatStr = '%(asctime)s %(message)s'
-    logging.basicConfig(format=formatStr, filename=fileName, level=logging.INFO)
-    myLogObj = logging.getLogger('sqa2023-logger') 
-    return myLogObj
+# Got rid of the method so I could re-use logger in multiple files with from myLogger import myLogObj
+fileName  = 'WAREAGLE_sqa2023_project.log' 
+formatStr = '%(asctime)s %(message)s'
+logging.basicConfig(format=formatStr, filename=fileName, level=logging.INFO)
+myLogObj = logging.getLogger('sqa2023-logger') 
